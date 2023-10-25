9dd90e7c30887951d9f9968e9c40dabdaeb337de
Changyoung Kwon <changyoung.kwon.0729@gmail.com>
2023-10-26 01:09:17 +0900

    test google signin

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react"
+import jwt_decode from "jwt-decode"
+
 export const Login: React.FC = () => {
-  throw "err";
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: "227413558974-lekbbtglqc3hfinte16re4cu8l7k5osh.apps.googleusercontent.com",
+      callback: res => {
+        const token = res.credential
+        console.log(jwt_decode(token))
+      }
+    })
+    google.accounts.id.renderButton(
+      document.getElementById("g_id_onload")!,
+      {theme: "outline", size: "large", type: "standard"}
+    )
+  }, [])
+  return <div id="g_id_onload" />
 }
